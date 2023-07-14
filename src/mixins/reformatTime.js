37bfd96e8c7981a59d059f8c1baf4053561112c3
Irene <irene@Irenes-Mac-mini.home>
2023-07-15 00:44:08 +0800

    update

--- src/mixins/reformatTime.js
+++ src/mixins/reformatTime.js
@@ -2,10 +2,7 @@ export default {
   methods: {
     // e.g.'12:00' (String) -> 1200 (Number)
     reformatTime(time) {
-      // const substrEnd =
-      //   time.indexOf('a') > -1 ? time.indexOf('a') : time.indexOf('p')
       return +time
-        // .substr(0, substrEnd)
         .split(':')
         .join('')
     }
