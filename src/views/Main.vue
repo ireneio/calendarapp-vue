591afc6294a83ed3f4bdca1e7f346f6a2f487400
Irene <irene@Irenes-Mac-mini.home>
2023-07-14 23:39:13 +0800

    update

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -83,7 +83,7 @@
       @selected-block="handleWeekBlockSelect($event)"
       @selected-block:day="handleDayBlockSelect($event)"
     ></router-view>
-    <div style="color: #181818; text-align: center; padding: 24px 0;">{{ `Copyright. Ires Consulting 2022-${'2023'}. All rights reserved.` }}</div>
+    <div style="color: #181818; text-align: center; padding: 24px 0; font-size: 12px;">{{ `Copyright. Ires Consulting 2022-${'2023'}. All rights reserved.` }}</div>
     <div
       class="container-xl position-fixed bg-white shadow-lg"
       style="{ transition: transform .66s; }"
